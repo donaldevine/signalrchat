6da8f9b359cc7f30c52d48de10b27c00ac38deec
Donal Devine <donaldevine@gmail.com>
2017-12-28 13:28:29 +0000

    Added Chat Hub

--- Startup.cs
+++ Startup.cs
@@ -15,6 +15,7 @@ namespace signalrchat
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +26,11 @@ namespace signalrchat
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseFileServer();
+
+            app.UseSignalR(
+                routes => {routes.MapHub<ChatHub>("chat");}
+            );
         }
     }
 }
